dda783d89c90e2862409978050288efc675e6261
ayam12g <115677760+ayam12g@users.noreply.github.com>
2025-05-31 12:30:12 +0700

    Update save-order.js

--- api/save-order.js
+++ api/save-order.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
   ]);
 
   if (error) {
-    console.error(error);
+    console.error("Supabase Error:", error.message);
     return res.status(500).json({ success: false });
   }
 
